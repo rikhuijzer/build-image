a6216c869a3945d159c31c17af649a2fb503e3e2
Rik <rik@huijzer.xyz>
2020-07-23 22:09:53 +0200

    Add julia 1.4

--- default.nix
+++ default.nix
@@ -53,6 +53,172 @@ let
   R-with-my-packages = pkgs.rWrapper.override{
     packages = my-r-packages;
   };
+
+  julia_14 = pkgs.stdenv.mkDerivation {
+    name = "julia_14";
+    src = pkgs.fetchurl {
+      url = "https://julialang-s3.julialang.org/bin/linux/x64/1.4/julia-1.4.2-linux-x86_64.tar.gz";
+      sha256 = "0a2w2q3ddggnsfxcxa9irzb23x7c7q8v3l00jzl101r64fz12wyp";
+    };
+    installPhase = ''
+      mkdir $out
+      cp -R * $out/
+
+      # Patch for https://github.com/JuliaInterop/RCall.jl/issues/339.
+
+      echo "patching $out"
+      cp -L ${pkgs.stdenv.cc.cc.lib}/lib/libstdc++.so.6 $out/lib/julia/
+    '';
+    dontStrip = true;
+    ldLibraryPath = with pkgs; stdenv.lib.makeLibraryPath [
+      stdenv.cc.cc
+      zlib
+      glib
+      xorg.libXi
+      xorg.libxcb
+      xorg.libXrender
+      xorg.libX11
+      xorg.libSM
+      xorg.libICE
+      xorg.libXext
+      dbus
+      fontconfig
+      freetype
+      libGL
+    ];
+  };
+  targetPkgs = pkgs: with pkgs; [
+    autoconf
+    curl
+    gnumake
+    utillinux
+    m4
+    gperf
+    unzip
+    stdenv.cc
+    clang
+    binutils
+    which
+    gmp
+    libxml2
+    cmake
+
+    fontconfig
+    openssl
+    which
+    ncurses
+    gtk2-x11
+    atk
+    gdk_pixbuf
+    cairo
+    xorg.libX11
+    xorg.xorgproto
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXext
+    xorg.libSM
+    xorg.libICE
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXdamage
+    xorg.libXrender
+    xorg.libXfixes
+    xorg.libXcomposite
+    xorg.libXcursor
+    xorg.libxcb
+    xorg.libXi
+    xorg.libXScrnSaver
+    xorg.libXtst
+    xorg.libXt
+    xorg.libXxf86vm
+    xorg.libXinerama
+    nspr
+    pdf2svg
+
+    # Nvidia note: may need to change cudnn to match cudatoolkit version
+    # cudatoolkit_10_0
+    # cudnn_cudatoolkit_10_0
+    # linuxPackages.nvidia_x11
+
+    julia_14
+
+    # Arpack.jl
+    arpack
+    gfortran.cc
+    (pkgs.runCommand "openblas64_" {} ''
+    mkdir -p "$out"/lib/
+    ln -s ${openblasCompat}/lib/libopenblas.so "$out"/lib/libopenblas64_.so.0
+    '')
+
+    # Cairo.jl
+    cairo
+    gettext
+    pango.out
+    glib.out
+    # Gtk.jl
+    gtk3
+    gtk2
+    fontconfig
+    gdk_pixbuf
+    # GR.jl # Runs even without Xrender and Xext, but cannot save files, so those are required
+    qt4
+    glfw
+    freetype
+
+    conda
+
+    #misc
+    xorg.libXxf86vm
+    xorg.libSM
+    xorg.libXtst
+    libpng
+    expat
+    gnome2.GConf
+    nss
+    
+  ];
+
+  env_vars = ''
+    export EXTRA_CCFLAGS="-I/usr/include"
+    
+    # Points RCall to `libR.so`.
+    export LD_LIBRARY_PATH="${pkgs.R}/lib/R/lib:$LD_LIBRARY_PATH"
+    # Ensure that RCall uses the same R version as used by `libR.so`.
+    # export R_HOME="${pkgs.R}/bin"
+
+    # This does not add dependencies (recursively)!
+    # Leaving the code since it was an interesting approach.
+    # export R_LIBS_USER="$<removed open curly bracket here>
+    #  (lib.concatMapStringsSep ":" (path: path + "/library") my-r-packages)
+    # }"
+    # export R_LIBS_USER=$R_LIBS_USER:$(R -e 'paste(.libPaths(), collapse=":")')
+
+    # This seems to run after nixos-rebuild-switch, so R knows all the packages.
+    # Do not set `R_LIBS_USER` since `using RCall` will overwrite it.
+    LIBRARIES=$(Rscript -e 'paste(.libPaths(), collapse=":")')
+    export R_LIBS_SITE="$(echo $LIBRARIES | cut -c6- | rev | cut -c2- | rev)"
+  '';
+  extraOutputsToInstall = ["man" "dev"];
+  multiPkgs = pkgs: with pkgs; [ zlib ];
+
+  julia-debug = pkgs.buildFHSUserEnv {
+    targetPkgs = targetPkgs;
+    name = "julia-debug"; # Name used to start this UserEnv
+    multiPkgs = multiPkgs;
+    runScript = "bash";
+    extraOutputsToInstall = extraOutputsToInstall;
+    profile = env_vars;
+  };
+
+  julia-fhs = pkgs.buildFHSUserEnv {
+    targetPkgs = targetPkgs;
+    name = "julia"; # Name used to start this UserEnv
+    multiPkgs = multiPkgs;
+    runScript = "julia";
+    extraOutputsToInstall = extraOutputsToInstall;
+    profile = env_vars;
+  };
+
 in pkgs.mkShell {
   name = "env";
   buildInputs = with pkgs; [
@@ -61,6 +227,7 @@ in pkgs.mkShell {
     glibcLocales # Avoids error in `devtools::check()`.
     haskellPackages.pandoc
     haskellPackages.pandoc-citeproc
+    julia-fhs
     R-with-my-packages
   ];
 }
